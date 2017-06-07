23f04183c5e9dc61a686176d17a4ae1cf92c9b72
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-07 13:24:18 -0300

    Config CSRF Token

--- s2b/core/static/core/js/app_client/app.js
+++ s2b/core/static/core/js/app_client/app.js
@@ -1,8 +1,14 @@
 (function () {
+    'use strict';
 
-    angular.module('s2bApp', ['ngRoute']);
+    angular.module('s2bApp', ['ngRoute', 'ngResource']);
 
-    function config ($routeProvider) {
+    function config ($routeProvider, $httpProvider, $resourceProvider) {
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+        
         $routeProvider
             .when('/', {
                 templateUrl: '/static/core/js/app_client/home/home.view.html',
@@ -12,6 +18,6 @@
 
     angular
         .module('s2bApp')
-        .config(['$routeProvider', config]);
+        .config(['$routeProvider', '$httpProvider', '$resourceProvider', config]);
 
 })();
