e7eea735b60dbddbcd9f2a754412b607f22b8324
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-07 14:53:33 -0300

    Agregando rutas y estructura del proyecto

--- s2b/core/static/core/js/app_client/app.js
+++ s2b/core/static/core/js/app_client/app.js
@@ -8,13 +8,30 @@
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 
         $resourceProvider.defaults.stripTrailingSlashes = false;
-        
+
         $routeProvider
             .when('/', {
                 templateUrl: '/static/core/js/app_client/home/home.view.html',
-                controller: 'homeCtrl'
+                controller: 'homeCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/about', {
+                templateUrl: '/static/core/js/app_client/about/about.view.html',
+                controller: 'aboutCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/register', {
+                templateUrl: '/static/core/js/app_client/auth/register/register.view.html',
+                controller: 'registerCtrl',
+                controllerAs: 'vm'
+            })
+            .when('/login', {
+                templateUrl: '/static/core/js/app_client/auth/login/login.view.html',
+                controller: 'loginCtrl',
+                controllerAs: 'vm'
             });
-}
+
+    }
 
     angular
         .module('s2bApp')
