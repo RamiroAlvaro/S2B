b9a795f3b1cf3fa7b4000734aef9d8584f809ef4
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-15 11:09:01 -0300

    Detail page

--- s2b/core/static/core/js/app_client/tripDetail/tripDetail.controller.js
+++ s2b/core/static/core/js/app_client/tripDetail/tripDetail.controller.js
@@ -11,8 +11,32 @@
         var vm = this;
 
         vm.pageHeader = {
-            title: 'DETAIL VIEW',
+            title: 'Funcionários --> Santa Tereza ',
             strapline: ''
+        };
+
+        vm.trip = {
+                id: 1,
+                origin: 'Funcionários',
+                destination: 'Santa Tereza',
+                origin_address: 'Av. Getúlio Vargas, 471',
+                destination_address: 'Rua Hermilo Alves, 85',
+                items: ['Vagas: 3', 'Homen', 'Honda Civic', 'Preto', 'HAO-1462'],
+                days: ['Segunda: 14:00 hs', 'Terça: 14:30 hs', 'Quarta: 14:00 hs','Quinta: 14:30 hs', 'Sexta: 14:00 hs'],
+                username: 'Ramiro Alvaro',
+                mensagens: [{
+                    author: 'Ana Paula Coda',
+                    createdOn: '26/06/2017',
+                    text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'
+                },{
+                    author: 'Pedro Paulo Gonçalves',
+                    createdOn: '24/06/2017',
+                    text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'
+                },{
+                    author: 'Mateus Hedegaard',
+                    createdOn: '15/06/2017',
+                    text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'
+                }]
         };
 
     }
