81d60a24bd53a7b80e587e23d7598eb2170f6e36
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-15 15:12:27 -0300

    Corrigiendo palabras

--- s2b/core/static/core/js/app_client/tripDetail/tripDetail.controller.js
+++ s2b/core/static/core/js/app_client/tripDetail/tripDetail.controller.js
@@ -21,10 +21,10 @@
                 destination: 'Santa Tereza',
                 origin_address: 'Av. Getúlio Vargas, 471',
                 destination_address: 'Rua Hermilo Alves, 85',
-                items: ['Vagas: 3', 'Homen', 'Honda Civic', 'Preto', 'HAO-1462'],
+                items: ['Vagas: 3', 'Homem', 'Honda Civic', 'Preto', 'HAO-1462'],
                 days: ['Segunda: 14:00 hs', 'Terça: 14:30 hs', 'Quarta: 14:00 hs','Quinta: 14:30 hs', 'Sexta: 14:00 hs'],
                 username: 'Ramiro Alvaro',
-                mensagens: [{
+                messages: [{
                     author: 'Ana Paula Coda',
                     createdOn: '26/06/2017',
                     text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor'
@@ -45,7 +45,7 @@
 
 
         vm.popupReviewForm = function () {
-            var modalInstance = $uibModal.open({
+            var uibModalInstance = $uibModal.open({
                 templateUrl: '/static/core/js/app_client/messageModal/messageModal.view.html',
                 controller: 'messageModalCtrl as vm',
 
