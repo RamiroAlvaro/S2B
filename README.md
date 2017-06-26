6cffcdde74e720aa309b0bd8ee96358b67aca36c
Ramiro Alvaro <ramiroalvaro.ra@gmail.com>
2017-06-26 08:17:26 -0300

    Actualizando README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Sistema de carpooling do S2B.
 
-[![Code Health](https://landscape.io/github/RamiroAlvaro/S2B-Pedro-Ramiro/master/landscape.svg?style=flat)](https://landscape.io/github/RamiroAlvaro/S2B-Pedro-Ramiro/master)
+[![Code Health](https://landscape.io/github/RamiroAlvaro/s2b/master/landscape.svg?style=flat)](https://landscape.io/github/RamiroAlvaro/s2b/master)
 
 ## Como desenvolver?
  
--- s2b/core/static/core/js/app_client/about/about.controller.js
+++ s2b/core/static/core/js/app_client/about/about.controller.js
@@ -12,7 +12,7 @@
             title: 'SOBRE NÓS'
         };
         vm.main = {
-            content: 'Está pensando em sair de casa e com aquela preguiça de esperar o ônibus? \n\n Existe um meio rápido e fácil para se locomover. O projeto S2B-P@@L vem com a praticidade e a rapidez afim de ser um diferencial na rotina diária das pessoas. Seja motorista ou caroneiro, interaja, avalie os usuários e tenha uma boa viagem!'
+            content: 'Está pensando em sair de casa e com aquela preguiça de esperar o ônibus? \n\n Existe um meio rápido e fácil para se locomover. O projeto S2B-P@@L vem com a praticidade e a rapidez a fim de ser um diferencial na rotina diária das pessoas. Seja motorista ou caroneiro, interaja, avalie os usuários e tenha uma boa viagem!'
         };
     }
 
